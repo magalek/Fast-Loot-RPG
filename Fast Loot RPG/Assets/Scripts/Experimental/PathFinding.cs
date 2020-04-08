d1c26e26eb9ca5a9c86539ef80cb4996f56e47b0
Filip <filipwalczak98@gmail.com>
2020-04-08 21:49:47 +0200

    Initial commit

---
+++ Fast Loot RPG/Assets/Scripts/Experimental/PathFinding.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Experimental {
+    public class PathFinding : MonoBehaviour
+    {
+        private void Start() {
+            FireRayCast();
+        }
+
+        private void FireRayCast() {
+            RaycastHit hit;
+            Physics.Raycast(transform.position, Target.Instance.transform.position - transform.position, out hit, 100);
+            // Debug.DrawRay(transform.position, transform.position - hit.point, Color.red);
+            Debug.DrawLine(transform.position, hit.point, Color.red);
+            Debug.Log(hit.point);
+            //if (!hit.collider.CompareTag("Player"))
+                
+        }
+    }
+}
---
+++ Fast Loot RPG/Assets/Scripts/Experimental/Target.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Experimental {
+    public class Target : MonoBehaviour {
+        public static Target Instance;
+        
+        // Start is called before the first frame update
+        void Awake() {
+            Instance = this;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
